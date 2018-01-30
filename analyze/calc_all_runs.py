ef227225ff3b5909cc090e59a1d41d5ab356b15e
JeroenvO <j.j.v.oorschot@student.tue.nl>
2018-01-30 18:58:00 +0000

    Measurements for long reactor.

--- analyze/calc_all_runs.py
+++ analyze/calc_all_runs.py
@@ -5,8 +5,8 @@ import os
 import importlib
 
 # these measurements contains bad ozone data.
-blacklist = ['calc_run_20180102.py', 'calc_run_20180103.py', 'calc_run_20180104.py']
-
+blacklist = ['calc_run_20180102.py', 'calc_run_20180103.py', 'calc_run_20180104.py']  # wrong ozone data
+blacklist += ['calc_run_20180105.py', 'calc_run_20180110.py', 'calc_run_20180111.py']  # wrong waveforms
 gbl = globals()
 for toImport in os.listdir('.'):
     if toImport[0:9] == 'calc_run_':
